da3d1f8113ca6ad9c062505a81c41f4236df954a
程浩伦 <chenghaolun@hualala.com>
2022-12-24 23:48:43 +0800

    feat: 创建页面/配置静态的路由表

--- src/layout/components/Siderbar/index.vue
+++ src/layout/components/Siderbar/index.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>SiderBar</div>
+  <div>
+    <h1>Hello_AlexCc</h1>
+    <el-scrollbar>
+      <sidebar-menu></sidebar-menu>
+    </el-scrollbar>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive } from 'vue'
+import SidebarMenu from './SidebarMenu.vue'
 </script>
 <style scoped></style>
